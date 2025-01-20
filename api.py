091a5d822e6de8226300926fb354c6b59f83133b
Lucas Gomes <lucas.danilo@portoseguro.com.br>
2025-01-20 16:33:25 -0300

    a

--- api.py
+++ api.py
@@ -55,4 +55,4 @@ def preencher_pdf():
         return {'error': f"Erro inesperado: {e}"}, 500
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True, port=6002)
